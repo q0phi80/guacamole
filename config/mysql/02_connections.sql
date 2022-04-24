65a9ecc7fe06e5b1e984be2b18f839768b89fff7
q0phi80 <lab.pwc@gmail.com>
2022-04-24 00:19:24 -0400

    Made a change to the connection file

--- config/mysql/02_connections.sql
+++ config/mysql/02_connections.sql
@@ -15,12 +15,12 @@
 use guacadb;
 
 -- Create connection
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Windows Server (ad - 10.0.1.4)', 'rdp');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Windows 10 (Initial Foothold)', 'rdp');
 INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Database Server (db - 10.0.1.6)', 'ssh');
 INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('OUD Server (oud - 10.0.1.5)', 'ssh');
 
 -- Add parameters to the Windows connection
-INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '10.0.1.4');
+INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '192.168.2.15');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'port', '3389');
 -- Add parameters to the Database connection
 INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', '10.0.1.6');
