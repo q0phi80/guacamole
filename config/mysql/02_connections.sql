54d9a84d88ab41e8771c59475d32578b06f3c3c6
q0phi80 <lab.pwc@gmail.com>
2022-04-24 17:24:42 -0400

    Changes made to the connection.sql

--- config/mysql/02_connections.sql
+++ config/mysql/02_connections.sql
@@ -16,16 +16,16 @@ use guacadb;
 
 -- Create connection
 INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Windows 10 (Initial Foothold)', 'rdp');
-INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Database Server (db - 10.0.1.6)', 'ssh');
+INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Kali (Attacker Box)', 'ssh');
 INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('OUD Server (oud - 10.0.1.5)', 'ssh');
 
 -- Add parameters to the Windows connection
-INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '192.168.2.15');
+INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '10.0.1.50');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'port', '3389');
 -- Add parameters to the Database connection
-INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', '10.0.1.6');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', '10.0.1.11');
 INSERT INTO guacamole_connection_parameter VALUES (2, 'port', '22');
-INSERT INTO guacamole_connection_parameter VALUES (2, 'username', 'oracle');
+INSERT INTO guacamole_connection_parameter VALUES (2, 'username', 'labuser');
 -- Add parameters to the OUD connection
 INSERT INTO guacamole_connection_parameter VALUES (3, 'hostname', '10.0.1.5');
 INSERT INTO guacamole_connection_parameter VALUES (3, 'port', '22');
