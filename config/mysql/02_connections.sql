93c9ef4bddf231dc946ffca651bce1bdbf82330e
q0phi80 <lab.pwc@gmail.com>
2022-07-17 15:59:36 -0400

    Update 02_connections.sql

--- config/mysql/02_connections.sql
+++ config/mysql/02_connections.sql
@@ -20,7 +20,7 @@ INSERT INTO guacamole_connection (connection_name, protocol) VALUES ('Kali (Atta
 -- Add parameters to the Windows connection
 INSERT INTO guacamole_connection_parameter VALUES (1, 'hostname', '10.0.1.53');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'port', '3389');
-INSERT INTO guacamole_connection_parameter VALUES (1, 'username', 'userall.user');
+INSERT INTO guacamole_connection_parameter VALUES (1, 'username', 'admin');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'password', 'Password@1');
 -- INSERT INTO guacamole_connection_parameter VALUES (1, 'domain', 'first');
 INSERT INTO guacamole_connection_parameter VALUES (1, 'security', 'any');
